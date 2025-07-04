1466337b13376c70cf96bfb5228f5b28ce67a4a9
Heet <heetnandu01@gmail.com>
2025-07-04 14:59:40 +0530

    minor changes

--- Arrays/leetcode136.java
+++ Arrays/leetcode136.java
@@ -5,5 +5,11 @@ package DSA.Arrays;
 
 
 public class leetcode136 {
-    
+     public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i = 0;i<nums.length;i++){
+            xor ^= nums[i];
+        }
+        return xor;
+    }
 }
