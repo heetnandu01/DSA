06f1351d6ecbef407db8d92e06dc7dee90a7557a
Heet <heetnandu01@gmail.com>
2025-07-12 19:36:31 +0530

    minor changes in the code

--- Arrays/leetcode1.java
+++ Arrays/leetcode1.java
@@ -8,16 +8,16 @@ import java.util.Map;
 
 public class leetcode1 {
      public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> num = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int complement = target  - nums[i];
-            if(num.containsKey(complement)){
-                return new int[] {num.get(complement),i};
-            }else{
-                num.put(nums[i], i);
+        Map<Integer,Integer> hash = new HashMap<>();
+
+        for(int i =0;i<nums.length;i++){
+            int comp = target -nums[i];
+            if(hash.containsKey(comp)){
+                return new int []{hash.get(comp),i};
             }
+            hash.put(nums[i],i);
         }
-        return new int[0]; 
+        return new int []{};
     }
 }
 
