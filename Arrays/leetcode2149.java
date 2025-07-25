ea14804ee3dd8f9d6c9a913a15b2146ba8c64790
Heet <heetnandu01@gmail.com>
2025-07-25 20:49:45 +0530

    made the solution better

--- Arrays/leetcode2149.java
+++ Arrays/leetcode2149.java
@@ -1,32 +1,22 @@
 package DSA.Arrays;
 
-import java.util.ArrayList;
-import java.util.List;
-
 //2149 - Rearrange Array Elements by sign
 //leetcode link:https://leetcode.com/problems/rearrange-array-elements-by-sign/description/
 
 public class leetcode2149 {
-     public int[] rearrangeArray(int[] nums) {
-        List<Integer> positives = new ArrayList<>();
-        List<Integer> negatives = new ArrayList<>();
-        int [] result = new int[nums.length];
-        for(int num:nums){
-            if(num > 0){
-                positives.add(num);
-            }else{
-                negatives.add(num);
+    public int[] rearrangeArray(int[] nums) {
+        int[] res = new int[nums.length];
+        int pos = 0;
+        int neg = 1;
+        for (int num : nums) {
+            if (num > 0) {
+                res[pos] = num;
+                pos += 2;
+            } else {
+                res[neg] = num;
+                neg += 2;
             }
         }
-        int posIndex = 0;
-        int negIndex = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(i % 2 == 0){
-                result[i] = positives.get(posIndex++);
-            }else{
-                result[i] = negatives.get(negIndex++);
-            }
-        }
-        return result;
+        return res;
     }
 }
